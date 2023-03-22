d7d9886ad0963c6a3c12da603f63aaadd7a1288e
Aniketbobade <bobadeaniket5@gmail.com>
2023-03-22 17:00:08 +0530

    course list

---
+++ src/App.js
@@ -0,0 +1,49 @@
+import React, { useEffect, useState } from "react";
+import Navbar from "./Components/Navbar";
+import Filter from "./Components/Filter";
+import Cards from "./Components/Cards";
+import { apiUrl, filterData } from "./data";
+import { toast } from "react-toastify";
+import Spinner from "./Components/Spinner";
+
+const App = () => {
+  const [courses, setCourses] = useState(null);
+  const [loading, setLoading] = useState(true);
+  const [category, setCategory] = useState(filterData[0].title);
+
+  async function fetchData() {
+    setLoading(true);
+    try{
+      let response = await fetch(apiUrl);
+      let output = await response.json();
+      ///output -> 
+      setCourses(output.data);
+    }
+    catch(error) {
+        toast.error("Network me koi dikkat hai");
+    }
+    setLoading(false);
+  }
+
+  useEffect(() => {
+    fetchData();
+  }, [])
+
+  return (
+    <div className="min-h-screen flex flex-col">
+      <Navbar />
+      <Filter filterData={filterData} 
+      category={category}
+      setCategory={setCategory}
+       />
+      <div className="w-11/12 max-w-[1200px] 
+        mx-auto flex flex-wrap justify-center items-center min-h-[50vh]">
+        {
+          loading ? (<Spinner/>) :(<Cards courses={courses} category={category}/>)
+        }
+      </div>
+    </div>
+  );
+};
+
+export default App;
---
+++ src/Components/Navbar.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+function Navbar() {
+  return (
+    <div>
+      <nav className="bg-blue-400 py-4">
+        <h2 className="text-3xl font-bold text-center text-white">Top course</h2>
+      </nav>
+    </div>
+  );
+}
+export default Navbar;
